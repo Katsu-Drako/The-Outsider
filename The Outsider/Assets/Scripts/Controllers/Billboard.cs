0953b4c2539bca537a5f0de070b44e397fee8c04
Fyttyn <fyttyn@gmail.com>
2019-11-26 17:53:13 -0500

    Billboarding

---
+++ The Outsider/Assets/Scripts/Controllers/Billboard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    protected virtual void OnEnable() {
+        Camera.onPreCull += FaceCamera;
+    }
+    protected virtual void OnDisable() {
+        Camera.onPreCull -= FaceCamera;
+    }
+
+    protected void FaceCamera(Camera camera) {
+        transform.LookAt(transform.position + Vector3.ProjectOnPlane(transform.position - camera.transform.position, Vector3.up).normalized, Vector3.up);
+    }
+}
